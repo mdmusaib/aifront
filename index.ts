e8ea3a61e97aeb229c50dd59ac8235497a368c0b
musaib <musaibkm@gmail.com>
2026-07-15 20:13:24 +0530

    set up husky

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-    console.log("Hello via Bun!")
+import concurrently from "concurrently";
+
+concurrently([
+  { command: "bun run index.ts", name: "server",cwd:"packages/server", prefixColor: "blue" },
+  { command: "bun run dev", name: "client",cwd:"packages/client", prefixColor: "green" }
+])
